52daaf3bc485fdc9bf804a4d89dfcf427d3ac57d
darkmift <darkmift@gmail.com>
2022-09-29 18:19:45 +0300

    migrated to new theme store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './App.scss'
 import Navbar from './components/layout/Navbar';
-
+import useThemeStore from './store/theme';
 
 import {
   MainRouter
 } from './router'
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const isDarkMode = useThemeStore(s => s.isDarkMode);
+  const setIsDarkMode = useThemeStore(s => s.setIsDarkMode);
 
   return (
     <div className={"app" + (isDarkMode ? ' dark-mode' : '')}>
